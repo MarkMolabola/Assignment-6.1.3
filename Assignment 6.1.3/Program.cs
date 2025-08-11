a887aae7a587acb758c8e0eabbfa67db8191e815
MarkMolabola <mark.molabola@outlook.com>
2025-08-11 14:24:11 -0700

    Add project files.

---
+++ Assignment 6.1.3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment_6._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 0, 1, 2, 0,3, 4, 5, 0,6, 7, 8, 9,0, 10, 0 };
+            
+            for (int i = 0, j=ints.Length-1 ; i < ints.Length; i++)
+            {
+                if (ints[i] == 0)
+                {
+                    for (int k = i; k < j; k++)
+                    {
+                        ints[k] = ints[k + 1];
+                    }
+                    ints[j] = 0;
+                    
+                }
+            }
+
+            foreach (var item in ints)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
