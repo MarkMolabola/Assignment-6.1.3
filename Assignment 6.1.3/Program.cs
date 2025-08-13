c8cd35b8543b6887735ef3bec55fdb15a3dd8847
MarkMolabola <mark.molabola@outlook.com>
2025-08-13 09:42:10 -0700

    update

--- Assignment 6.1.3/Program.cs
+++ Assignment 6.1.3/Program.cs
@@ -5,25 +5,40 @@
         static void Main(string[] args)
         {
             int[] ints = { 0, 1, 2, 0,3, 4, 5, 0,6, 7, 8, 9,0, 10, 0 };
-            
-            for (int i = 0, j=ints.Length-1 ; i < ints.Length; i++)
+            MoveTheZeros(ints);
+
+
+
+        }
+        static void MoveTheZeros(int[] ints)
+        {
+            //for (int i = 0, j = ints.Length - 1; i < ints.Length; i++)
+            //{
+            //    if (ints[i] == 0)
+            //    {
+            //        for (int k = i; k < j; k++)
+            //        {
+            //            ints[k] = ints[k + 1];
+            //        }
+            //        ints[j] = 0;
+
+            //    }
+            //}
+
+            //return ints;
+            int count = 0;
+            for (int i = 0; i < ints.Length; i++)
             {
-                if (ints[i] == 0)
+                if (ints[i] != 0)
                 {
-                    for (int k = i; k < j; k++)
-                    {
-                        ints[k] = ints[k + 1];
-                    }
-                    ints[j] = 0;
-                    
+                    ints[count++] = ints[i];
                 }
             }
-
-            foreach (var item in ints)
+            while (count < ints.Length)
             {
-                Console.Write(item + " ");
+                ints[count++] = 0;
             }
-
+            
         }
     }
 }
